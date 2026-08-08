98a3fe4604b5480485593ee10acb4ce24411c940
Lisa <lisa1105ice@gmail.com>
2026-08-08 11:14:05 +0900

    q

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+# import streamlit as st
+
+# st.title("ユーザー情報入力")
+
+# if "user_name" not in st.session_state:
+#     st.session_state.user_name=""
+
+#     name=st.text_input("あなたの名前を入力してください")
+#     if st.button("名前を保存"):
+#         st.session_state.user_name=name
+#         st.success("名前を保存しました！")
+
+# st.write(f"現在保存されている名前：{st.session_state.user_name}")
+
 import streamlit as st
 
 st.title("ユーザー情報入力")
 
-if "user_name" not in st.session_state:
-    st.session_state.user_name=""
+# session_stateの初期化
+if 'user_name' not in st.session_state:
+    st.session_state.user_name = ""
 
-    name=st.text_input("あなたの名前を入力してください")
-    if st.button("名前を保存"):
-        st.session_state.user_name=name
-        st.success("名前を保存しました！")
-
-st.write(f"現在保存されている名前：{st.session_state.user_name}")
+# ユーザー名の入力
+name = st.text_input("あなたの名前を入力してください")
+if st.button("名前を保存"):
+    st.session_state.user_name = name
+    st.success("名前を保存しました!")
+    
+st.write(f"現在保存されている名前: {st.session_state.user_name}")
